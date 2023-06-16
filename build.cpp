4dc907cf6ee1aeeaef3acee54c989aad1c91777d
Eduardo Costa <m4c0@users.noreply.github.com>
2023-06-16 13:03:43 +0100

    poc: load image using hai for... RAII...

--- build.cpp
+++ build.cpp
@@ -1,9 +1,17 @@
 #include "../ecow/ecow.hpp"
+#include "../hai/build.hpp"
+#include "../missingno/build.hpp"
+#include "../silog/build.hpp"
+#include "../sires/build.hpp"
 
 int main(int argc, char **argv) {
   using namespace ecow;
 
   auto poc = unit::create<tool>("poc");
+  poc->add_wsdep("hai", hai());
+  poc->add_wsdep("missingno", missingno());
+  poc->add_wsdep("silog", silog());
+  poc->add_wsdep("sires", sires());
   poc->add_unit<>("poc");
   return run_main(poc, argc, argv);
 }
--- poc.cpp
+++ poc.cpp
@@ -1,18 +1,39 @@
+import hai;
+import silog;
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+
+namespace stbi {
+static constexpr const auto num_channels = 4;
+
+struct deleter {
+  void operator()(unsigned char *ptr) { stbi_image_free(ptr); }
+};
+using uc_ptr = hai::value_holder<unsigned char *, deleter>;
+
+struct image {
+  int width;
+  int height;
+  int num_channels;
+  uc_ptr data;
+};
+
+image load(const char *fname) {
+  image res{};
+  res.data = uc_ptr{stbi_load(fname, &res.width, &res.height, &res.num_channels,
+                              num_channels)};
+  return res;
+}
+} // namespace stbi
 
 int main(int argc, char **argv) {
   if (argc < 2)
     return 1;
 
-  auto fname = argv[1];
-
-  static constexpr const auto num_channels = 4;
-
-  int w;
-  int h;
-  int n;
-  auto raw = stbi_load(fname, &w, &h, &n, num_channels);
-  if (raw == nullptr)
+  auto img = stbi::load(argv[1]);
+  if (*img.data == nullptr)
     return 1;
+
+  silog::log(silog::info, "Image: %dx%d", img.width, img.height);
 }
